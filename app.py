b018a9f901adcc4f45af6c32e0522a5bbe4e348b
ravi <ravi.chinnamanaidu@gmail.com>
2020-12-05 10:06:02 -0500

    files updated

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from sqlalchemy import create_engine
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import time
+import csv
+import requests
+from scipy import stats
 
 # Create an instance of Flask
 app = Flask(__name__)
@@ -23,24 +26,86 @@ def home():
 
     # Find one record of data from the mongo database
     # @TODO: YOUR CODE HERE!
-    data_cnt = engine.execute("select * from country ", con=engine)
+    country = engine.execute("select * from country ", con=engine)
     data_cat = engine.execute("select * from satellite_category", con=engine)
     #return render_template("index.html", listings=listings)
     # Return template and data
-    return render_template("index.html", country=data_cnt, category=data_cat)
+    testValTestOnly = {'firstname': 'Harry', 'lastname': 'Potter'}
+    resdata = {
+        "lat":"test1",
+        "lng":"test2"
+    }
+    return render_template("index.html", country=country, category=data_cat, testVal = testValTestOnly,resdata=resdata)
 
+
+#
+#@app.route("/api/v1.0/<startdt>/<enddt>")
+#def startEndDate(startdt, enddt):
 
 # Route to render index.html template using data from Mongo
-@app.route("/getSatellite?cnt=US")
-def satellite_country():
+@app.route("/getSatellite/<cntry>")
+def satellite_country(cntry):
 
     # Find one record of data from the mongo database
     # @TODO: YOUR CODE HERE!
-    sat_cnt_cnt = engine.execute("select * from country_satellite where country_code=\'US\'", con=engine)
+    sat_cnt_cnt = engine.execute("select * from country_satellite where country_code='"+cntry+"'", con=engine)
     data_cat = engine.execute("select * from satellite_category", con=engine)
+    
+    country = engine.execute("select * from country ", con=engine)
     #return render_template("index.html", listings=listings)
     # Return template and data
-    return render_template("index.html", country=sat_cnt_cnt, category=data_cat)
+
+    incrd =0
+    coordinatesjson = {}
+    resdata = [{
+  
+    }
+    ]
+
+    responsedata = { 'respdata': resdata
+    }
+
+    incdata = 0
+    
+    for record in sat_cnt_cnt:
+        coordinatesjson = {}
+        try: 
+            url = "https://api.n2yo.com/rest/v1/satellite/positions/"+str(record['satellite_id'])+"/41.702/-76.014/0/2/&apiKey=J3H9EJ-Z2GE6Y-BC2E6G-4LOF"
+            response = requests.get(url).json()            
+            #print(response)
+            print(response['positions'][0]['satlatitude'])
+            print(response['positions'][0]['satlongitude'])
+            print(response['positions'])
+            print(response['info']['satname'])
+            print(response['info']['satid'])
+            print(response['positions'][0]['azimuth'] )
+            print(response['positions'][0]['elevation'] )
+            print(response['positions'][0]['sataltitude']   )
+            #print(response['info'])
+          #  coordinates.append(response['positions'][0]['satlatitude'])
+          #  coordinates.append(response['positions'][0]['satlongitude'])
+            coordinatesjson['latitude'] = response['positions'][0]['satlatitude']            
+            coordinatesjson['longitude'] = response['positions'][0]['satlongitude']                      
+            #coordinatesjson['azimuth'] = response['positions'][0]['azimuth']            
+            #coordinatesjson['elevation'] = response['positions'][0]['elevation']            
+            #coordinatesjson['altitude'] = response['positions'][0]['sataltitude']            
+            #coordinatesjson['satname'] = response['info']['satname']            
+            #coordinatesjson['satid'] = response['info']['satid']  
+            resdata.append(coordinatesjson)
+            incrd = incrd+1
+            incdata = incdata+1
+            if (incrd >10):
+                break
+        except:
+            pass
+
+        responsedata['respdata'] = resdata
+    
+    testValTestOnly = {'firstname': 'Harry', 'lastname': 'Potter'}
+
+    # Build partial query URL
+   
+    return render_template("index2.html", country=country, category=data_cat, sat_country=sat_cnt_cnt,resdata=responsedata,testVal = testValTestOnly)
 
 
 
--- static/plot.js
+++ static/plot.js
@@ -13,20 +13,22 @@ var mapZoomLevel = 8;
 // Adding tile layer
 
 // Load in GeoJson data
-var geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+//var geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 //var tecPlates = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json";
 
-var tecPlates = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
+//var tecPlates = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
+
 
 var myMap = L.map("map", {
     center: [32.7488333,-115.814],
     zoom: mapZoomLevel
    });
+   
 
-var geojson;
 //d3.json(geoData, creaetMarkers);
-var satelite = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+
+var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 18,
@@ -35,37 +37,137 @@ var satelite = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/
   accessToken: API_KEY
 });
 
+
 //.addTo(myMap);
 // Streetmap Layer
 
 
-var grayscale = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "dark-v10",
-  accessToken: API_KEY
-});
 
-var outdoor = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "dark-v11",
-  accessToken: API_KEY
-});
 
-var erthQuakeMarkers = [];
-var tectonicLines = [];
-
-var inputElement = d3.select("#selDatasetCnt");
-
+//var inputElement = d3.select("#selDatasetCnt");
+//var inputValueCnt = inputElement.property("value");
+var inputValueCnt ="US";
 function optionCntChanged(inputValue) {
+  console.log("data_cnt");
+  console.log(testVal);
+  console.log("resdata");
+  console.log(resdata);
 
+  if (resdata != '') {
+  var geocode1 = JSON.parse(resdata);
+  console.log(geocode1['respdata']);
+  console.log(geocode1['respdata'].length);
+  console.log(geocode1['respdata'][1]);
+  console.log(geocode1['respdata'][1]['latitude']);
+  console.log(geocode1['respdata'][1]['longitude']);
+  console.log("country");
+  console.log(testVal);
+  console.log("category");
+  console.log(testVal);
   console.log("inputElement");
-  console.log(inputElement);
 
-  
+  //var inputValueCnt = inputElement.property("value");
+  //console.log(inputValueCnt);
+  console.log("inputValue");
+  console.log(inputValue);
+  cl="green"
+  opac=0.9
+  var stateMarkers = [];
+  var geocode = JSON.parse(resdata);
+  for (var i=1; i<geocode['respdata'].length; i++) {
+    console.log("This is within the for condition");
+    console.log(geocode['respdata'][i]);
+   
+    stateMarkers.push(
+      L.circle([geocode['respdata'][i]['latitude'],geocode['respdata'][i]['longitude']], {
+        fillOpacity: opac,
+        color: cl,
+        fillColor: "black",
+        radius: 4*1000
+      }).bindPopup("<h3> latitude:" + geocode['respdata'][i]['latitude'] + "</h3> <br> <h3>longitude: " + geocode['respdata'][i]['longitude'] + "</h3><br>"));
+                   
+                   /*
+                   .bindPopup("<h3> latitude:" + geocode['respdata'][i]['latitude'] + "</h3> <br> "+
+                    "<h3>longitude: " + geocode['respdata'][i]['longitude'] + "</h3><br>"))
+                     "<h3>satname: " + geocode['respdata'][i]['satname'] + "</h3><br> "+
+                    "<h3>satid: " + geocode['respdata'][i]['satid'] + "</h3><br> "+
+                    "<h3>azimuth: " + geocode['respdata'][i]['azimuth'] + "</h3><br> "+
+                    "<h3>elevation: " + geocode['respdata'][i]['elevation'] + "</h3><br> "+
+                    "<h3>altitude: " + geocode['respdata'][i]['altitude'] + "</h3><br>"));
 
-  d3.json("samples.json", function (response) {
+                    altitude: 36083.24
+azimuth: 54.9
+elevation: -51.42
+latitude: -10.46726356
+longitude: 68.71750747
+satid: 23948
+satname: "ARABSAT 2A"
+                    */
+    
+    /*
+  L.circle([geocode['respdata'][i]['latitude'],geocode['respdata'][i]['longitude']], {
+    fillOpacity: opac,
+    color: "black",
+    fillColor: cl,
+    // Setting our circle's radius equal to the output of our markerSize function
+    // This will make our marker's size proportionate to its population
+    radius: 4*10000
+  }).bindPopup("<h1> Sat Country:" + inputValue + "</h1> <hr> <h3>Sat Name: " + inputValue+ "</h3>").addTo(myMap);
+  */
+}
+
+console.log(stateMarkers);
+
+var states = L.layerGroup(stateMarkers);
+// Create an overlay object
+var overlayMaps = {
+  "State Population": states
+};
+var baseMaps = {
+  "Street Map": streetmap
+};
+// Define a map object
+//streetmap.addTo(myMap);
+//states.addTo(myMap);
+/*
+var myMap = L.map("map", {
+  center: [-115.814, 32.7488333],
+  zoom: 12,
+  layers: [ streetmap, states]
+});
+*/
+
+L.control.layers( baseMaps, overlayMaps, {
+  collapsed: true
+}).addTo(myMap);
+
+
+  //d3.select(" .my-link>a").attr("href", "https://nytimes.com").text("Now this is the link for NYT");
+
+  //window.for("/getSatellite/"+inputValue);
+
+}
+
+}
+
+console.log("data_cnt");
+console.log(testVal);
+//console.log("data_cat");
+//console.log(resdata);
+
+
+optionCntChanged(inputValueCnt);
+ 
+
+// Use the map method with the arrow function to return all the filtered movie titles.
+//var titles = filteredMovies.map(movies =>  movies.title);
+
+// Use the map method with the arrow function to return all the filtered movie metascores.
+//var ratings = filteredMovies.map(movies => movies.metascore);
+
+
+
+/*
     //console.log(response);
     //console.log(response.features[0]);
     //console.log(response.features.length);
@@ -92,21 +194,6 @@ function optionCntChanged(inputValue) {
     }
 
   });
-
-}
-
-   
-optionCntChanged("US");
- 
-
-// Use the map method with the arrow function to return all the filtered movie titles.
-//var titles = filteredMovies.map(movies =>  movies.title);
-
-// Use the map method with the arrow function to return all the filtered movie metascores.
-//var ratings = filteredMovies.map(movies => movies.metascore);
-
-
-
-
+*/
 
 
