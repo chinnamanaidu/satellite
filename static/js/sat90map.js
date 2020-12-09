57135ce0125a44124fade5edcf622eef0f1aefd6
Asrit Caceres <asritcaceres@gmail.com>
2020-12-09 00:35:45 -0500

    adding nav bars

--- static/js/sat90map.js
+++ static/js/sat90map.js
@@ -2,7 +2,7 @@
 var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
-    maxZoom: 18,
+    maxZoom:3,
     zoomOffset: -1,
     id: "mapbox/streets-v11",
     accessToken: API_KEY
@@ -18,7 +18,7 @@ var layers = {
 // // Create map & boundaries
 var myMap = L.map("map", {
     center: [40.54055668158878, -104.35709913347607],
-    zoom: 6,
+    zoom: 2,
     layers: [
         layers.Under_10K,
         layers.From_10Kto50K,
