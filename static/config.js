c9a52a5ae9fdd38d8b5af92a283c1f17e543d91b
tsotetsi <kholiswa.tsotetsi@gmail>
2020-12-08 21:41:45 -0500

    merge data

--- static/config.js
+++ static/config.js
@@ -4,6 +4,8 @@
 //const API_KEY ="pk.eyJ1IjoiY2hpbm5hbWFuYWlkdSIsImEiOiJja2ljMG5hM3owMTd5MnFrOGF3NGhxeTJhIn0.ANPMzk6pFMoSyZtDMRtlKQ"
 //const API_KEY = "pk.eyJ1IjoiY2hpbm5hbWFuYWlkdSIsImEiOiJja2liejdnamswMHRoMnFrOG43NjQ4dzhtIn0.74pfJspKfTsmPWeXeCpRTA"
 
-const API_KEY ="pk.eyJ1IjoicmF2aWNoYW5kcmFuY2hpbm4iLCJhIjoiY2tpYzFhM3g0MDgxZDJ4cnp3aDI5c3lzaiJ9.60Cm0YZ4p4Da0xNUMtxZhA"
+// const API_KEY ="pk.eyJ1IjoicmF2aWNoYW5kcmFuY2hpbm4iLCJhIjoiY2tpYzFhM3g0MDgxZDJ4cnp3aDI5c3lzaiJ9.60Cm0YZ4p4Da0xNUMtxZhA"
+
+const API_KEY = "pk.eyJ1Ijoia3Rzb3RldCIsImEiOiJja2htbjFiYm8wOG1oMzByZG90dGFwNXRkIn0.RWInQflB7naTkOdV9nW0nw"
 
 const G_KEY = ""
